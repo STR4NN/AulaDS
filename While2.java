b796ef6f03ff5cea1e66fc32eea526acbc80b9ee
Gustavo Lopes <126016934+STR4NN@users.noreply.github.com>
2024-03-28 15:19:56 -0300

    EXERCICIO WHILE

---
+++ While2.java
@@ -0,0 +1,18 @@
+public class While2 {
+    public static void main(String[] args) {
+
+
+                int numero = 1;
+                while (numero <= 100){
+                    int valor  = numero++;
+
+                    if (numero %2 == 1){
+
+                        System.out.println("Numeros pares até 100: " + valor);
+                    }
+
+
+                }
+            }
+        }
+
